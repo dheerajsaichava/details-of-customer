1a53f2f418502c668b66eaacda366a25b1ede404
dheerajsai chava <dheerajsaichava@gmail.com>
2019-09-10 16:14:12 -0400

    Initial Commit

---
+++ details of customer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace details_of_customer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("what is ur name");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("what is your age");
+            var age = Console.ReadLine();
+
+            Console.WriteLine("what is your birth month");
+            var month = Console.ReadLine();
+
+            if(month=="july")
+
+            {
+                Console.WriteLine("you are a cancer");
+            }
+        }
+    }
+}
